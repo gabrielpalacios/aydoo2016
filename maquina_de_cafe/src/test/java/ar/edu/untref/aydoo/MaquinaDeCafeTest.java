03248512a0ea0c85492adb21143e8b001ed5ea0c
gabrielpalacios <palaciosgabriel@hotmail.com>
2016-03-29 16:18:45 -0300

    29-3tarde

--- maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/MaquinaDeCafeTest.java
+++ maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/MaquinaDeCafeTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MaquinaDeCafeTest {
+	
+
 	@Test
 	public void testCafeteroSoloAgregoCafe(){
 		
@@ -27,6 +29,18 @@ public class MaquinaDeCafeTest {
 	}
 	
 	@Test
+	public void testExpendedoraPreparoCafeConLeche0DeAzucar (){
+		
+		MaquinaExpendedoraDeBebidas expendedora = new MaquinaExpendedoraDeBebidas();
+		Vaso vaso = expendedora.hacerCafeConLecheConNDeAzucar(0);
+		
+		
+		Assert.assertTrue("Cafetero puso cafe al vaso", vaso.getCafe());
+		Assert.assertTrue("Lechero puso leche al vaso", vaso.getLeche());
+		Assert.assertEquals("Azucarero puso 0 de azucar al vaso", 0, vaso.getAzucar());
+	}
+	
+	@Test
 	public void testExpendedoraPreparoCafeConLeche4DeAzucar (){
 		
 		MaquinaExpendedoraDeBebidas expendedora = new MaquinaExpendedoraDeBebidas();
@@ -37,5 +51,17 @@ public class MaquinaDeCafeTest {
 		Assert.assertTrue("Lechero puso leche al vaso", vaso.getLeche());
 		Assert.assertEquals("Azucarero puso 4 de azucar al vaso", 4, vaso.getAzucar());
 	}
+	
+	@Test
+	public void testFalsaAzucarAlVaso(){
+		Vaso vaso = new Vaso();
+		Azucarero azucarero =  new Azucarero();
+		
+		azucarero.ponerNEnEsteVaso (0, vaso);
+		
+		Assert.assertEquals("Azucarero no agrega azucar", 0, vaso.getAzucar());
+	}
+	
+	
 	
 }
