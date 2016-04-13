b4e527f9c45075835aaf8e429be51ea15c841bf9
gabrielpalacios <palaciosgabriel@hotmail.com>
2016-04-13 01:09:58 -0300

    Delete Libro.java

--- ejercicio_kiosco/src/Libro.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.untref.aydoo;
-
-public class Libro extends Producto {
-	//por ahora solo constructor:
-	public Libro(String nombre, Double precio) throws Exception {
-        //		llamo al Super!
-		super(nombre, precio);
-	}
-}
